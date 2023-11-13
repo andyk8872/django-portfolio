51cef8ab7b8dc99a90ec24a926d02463c0559b56
Andrew Kennedy <92186003+andyk8872@users.noreply.github.com>
2023-11-13 20:09:23 +0000

    add and link postgresql

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -81,10 +81,21 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ehbejhfo', 
+        'USER': 'ehbejhfo',
+        'PASSWORD': 'EuL_XX-ZACXxekU2jcT1uQvrKGhrI-oM',
+        'HOST': 'flora.db.elephantsql.com', 
+        'PORT': '5432',
     }
 }
 
